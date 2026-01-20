637053a05244925cb13b0f10ea84f43c95bcd8ca
polgarattila <polgarattila.dev@gmail.com>
2026-01-20 03:16:14 +0100

    set up JavaFX TableView with database integration

--- src/main/java/com/restaurant/system/restaurantmanagingsystem/HelloController.java
+++ src/main/java/com/restaurant/system/restaurantmanagingsystem/HelloController.java
@@ -1,14 +1,41 @@
 package com.restaurant.system.restaurantmanagingsystem;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.List;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TableView<MenuItem> menuTable;
+
+    @FXML
+    private TableColumn<MenuItem, String> nameColumn;
+
+    @FXML
+    private TableColumn<MenuItem, Double> priceColumn;
+
+    @FXML
+    private TableColumn<MenuItem, String> categoryColumn;
+
+    public void initialize() {
+        // Megmondjuk az oszlopoknak, hogy a MenuItem melyik mezőjét mutassák
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+        categoryColumn.setCellValueFactory(new PropertyValueFactory<>("categoryName"));
+
+        // Adatok betöltése az adatbázisból
+        loadMenuItems();
+    }
+
+    private void loadMenuItems() {
+        List<MenuItem> items = DatabaseConnection.getMenuItemsForGUI();
+        ObservableList<MenuItem> observableList = FXCollections.observableArrayList(items);
+        menuTable.setItems(observableList);
     }
 }
